bf0ea7c02a0baf41fbe2b30c15945e3903c023bf
tomasperez99 <perezvitale.tomas@gmail.com>
2021-06-27 14:06:43 -0300

    Se importa StarRating

--- src/components/Resenia.vue
+++ src/components/Resenia.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>
-        
+    <div class="divPadre">
+        <p class="pTittle" align="left">Escribe tu reseña sobre {Trabajador.nombre}</p>
+        <br>
+        <br>
+        <star-rating v-model="rating"></star-rating>
     </div>
 </template>
 <script>
+import StarRating from 'vue-star-rating'
 export default {
-    
+    components: {
+        StarRating
+    }
 }
 </script>
+
+<style>
+
+    .divPadre{
+        border-style: solid;
+        border-width: 1px;
+        border-color: rgb(192, 192, 192);
+        box-shadow: 2px 3px #e0e0e0;
+    }
+
+    .pTittle{
+        padding-left: 45px;
+        padding-top: 15px;
+        font-weight: bold;
+    }
+
+</style>
