891cbf18c74c2668ebe46505d1715d7049f21ac8
Ema <ema_abitante@hotmail.com>
2021-06-28 19:57:40 -0300

    Estadisticas terminado

--- src/components/Estadisticas.vue
+++ src/components/Estadisticas.vue
@@ -1,17 +1,54 @@
 <template>
-    <div>
-        <!-- <div>{{this.trabajadores}}</div>
-        <div>{{this.rubros}}</div>
-        <div>{{this.resenias}}</div>
-        <div>{{this.solicitantes}}</div>
-        <div>{{this.solicitudes}}</div> -->
-        
-        <div>Rubro mas solicitado: {{this.getFromList('id', this.rubros, this.rubroMasSolicitado).nombre}}</div>
-        <div>Rubro menos solicitado: {{this.getFromList('id', this.rubros, this.rubroMenosSolicitado).nombre}}</div>
-        <div>{{this.trabajadoresConMasSolicitudes}}</div>
-        <div>{{this.solicitantesMasActivos}}</div>
-        <div>{{this.trabajadoresMejorRating}}</div>
+    <div class="container">
+    <div class="heading">
+      <h1>Estadisticas</h1>
     </div>
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          <h2>Trabajadores mas solicitados</h2>
+        </div>
+        <div class="card-body" v-for="trabajador in trabajadoresConMasSolicitudes" :key="trabajador">
+            <label>{{trabajador.nombre}}</label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h2>Trabajadores mejor calificados</h2>
+        </div>
+        <div class="card-body" v-for="trabajadorr in trabajadoresMejorRating" :key="trabajadorr">
+            <label>{{trabajadorr.nombre}}</label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h2>Solicitantes con mas solicitudes Realizadas</h2>
+        </div>
+        <div class="card-body" v-for="solicitante in solicitantesMasActivos" :key="solicitante">
+            <label>{{solicitante.nombre}}</label>
+        </div>
+      </div>
+    </div>
+    <div class= "row2">
+        <div class="card">
+        <div class="card-header">
+          <h2>Rubro mas solicitado</h2>
+        </div>
+        <div class="card-body" >
+            <label>{{getFromList('id', this.rubros, rubroMasSolicitado).nombre}}</label>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h2>Rubro menos solicitado</h2>
+        </div>
+        <div class="card-body" >
+            <label>{{getFromList('id', this.rubros, rubroMenosSolicitado).nombre}}</label>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 <script>
 import axios from 'axios'
@@ -30,9 +67,12 @@ export default {
             rubros: [],
             rubroMasSolicitado: null,
             rubroMenosSolicitado: null,
+            IDTrabajadoresConMasSolicitudes: [],
             trabajadoresConMasSolicitudes: [],
             trabajadoresMejorRating: [],
-            solicitantesMasActivos: []
+            IDTrabajadoresMejorRating: [],
+            IDSolicitantesMasActivos: [],
+            solicitantesMasActivos:[]
         }
     },
     methods: {
@@ -42,16 +82,30 @@ export default {
             this.getTopSolicitantes()
             this.getTopsRubros()
             this.getTrabajadoresRating()
+            this.map()
+        },
+
+        map() {
+            let i = 0;
+            for (i = 0; i < this.IDTrabajadoresConMasSolicitudes.length; i++) {
+                this.trabajadoresConMasSolicitudes.push(this.getFromList('id', this.trabajadores, this.IDTrabajadoresConMasSolicitudes[i]))
+            }
+            for (i = 0; i < this.IDSolicitantesMasActivos.length; i++) {
+                this.solicitantesMasActivos.push(this.getFromList('id', this.solicitantes, this.IDSolicitantesMasActivos[i]))
+            }
+            for (i = 0; i < this.IDTrabajadoresMejorRating.length; i++) {
+                this.trabajadoresMejorRating.push(this.getFromList('id', this.trabajadores, this.IDTrabajadoresMejorRating[i]))
+            }
         },
         
         getTopTrabajadores() {
             let frecuenciaTrabajadores = this.getFrecuenciasArray(this.solicitudes, this.trabajadores, 'idTrabajador')
-            this.trabajadoresConMasSolicitudes = this.getTopFromArray(frecuenciaTrabajadores, 3)
+            this.IDTrabajadoresConMasSolicitudes = this.getTopFromArray(frecuenciaTrabajadores, 3)
         },
 
         getTopSolicitantes() {
             let frecuenciaSolicitantes = this.getFrecuenciasArray(this.solicitudes, this.solicitantes, 'idSolicitante')
-            this.solicitantesMasActivos = this.getTopFromArray(frecuenciaSolicitantes, 2)
+            this.IDSolicitantesMasActivos = this.getTopFromArray(frecuenciaSolicitantes, 2)
         },
 
         getTrabajadoresRating() {
@@ -82,7 +136,7 @@ export default {
 
             console.log(ratingsTrabajadores)
             console.log(aux)
-            this.trabajadoresMejorRating = this.getTopFromArray(aux, 3)
+            this.IDTrabajadoresMejorRating = this.getTopFromArray(aux, 3)
 
         },
         
@@ -200,6 +254,83 @@ export default {
     }
 }
 </script>
-<style lang="">
-    
+<style>
+    * {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  background: #fefefe;
+  font-family: sans-serif;
+}
+.container {
+  width: 200%;
+  margin: 50px auto;
+}
+.heading {
+  text-align: center;
+  font-size: 20px;
+  margin-bottom: 50px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-flow: wrap;
+}
+.row2 {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  flex-flow: wrap;
+}
+.card {
+  width: 21%;
+  background: #fff;
+  border: 1px solid #ccc;
+  margin-bottom: 50px;
+  transition: 0.3s;
+}
+.card-header {
+  text-align: center;
+  padding: 10px 10px;
+  background: linear-gradient(to right, #ff416c, #ff4b2b);
+  color: #fff;
+}
+.card-body {
+  padding: 18px 15px;
+  text-align: center;
+  font-size: 25px;
+}
+.card-body .btn {
+  display: block;
+  color: #fff;
+  text-align: center;
+  background: linear-gradient(to right, #ff416c, #ff4b2b);
+  margin-top: 30px;
+  text-decoration: none;
+  padding: 10px 5px;
+}
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.25);
+}
+@media screen and (max-width: 1000px) {
+  .card {
+    width: 40%;
+  }
+}
+@media screen and (max-width: 620px) {
+  .container {
+    width: 100%;
+  }
+  .heading {
+    padding: 20px;
+    font-size: 20px;
+  }
+  .card {
+    width: 80%;
+  }
+}
 </style>
